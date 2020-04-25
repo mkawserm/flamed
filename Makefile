e81166d3e0b9b6416c93e92439274de148a6eb1e
kawser <mkawserm@gmail.com>
2020-04-25 16:25:39 +0600

    FLAMED SNAPSHOT PROTO ADDED

---
+++ Makefile
@@ -0,0 +1,8 @@
+cover:
+	@go test -coverprofile=cover.out -v
+
+cover-html:
+	@go tool cover -html=cover.out
+
+protobuf:
+	@protoc -I=./pkg/pb --go_out=. flamedsnapshot.proto
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,35 +2,3 @@ package storage
 
 type Storage struct {
 }
-
-func (s *Storage) Create(namespace []byte, key []byte, value []byte) (bool, error) {
-	return true, nil
-}
-
-func (s *Storage) Update(namespace []byte, key []byte, value []byte) (bool, error) {
-	return true, nil
-}
-
-func (s *Storage) Delete(namespace []byte, key []byte) (bool, error) {
-	return true, nil
-}
-
-func (s *Storage) Read(namespace []byte, key []byte) ([]byte, error) {
-	return nil, nil
-}
-
-func (s *Storage) CreateMeta(namespace []byte, data []byte) (bool, error) {
-	return false, nil
-}
-
-func (s *Storage) UpdateMeta(namespace []byte, data []byte) (bool, error) {
-	return false, nil
-}
-
-func (s *Storage) DeleteMeta(namespace []byte) (bool, error) {
-	return false, nil
-}
-
-func (s *Storage) ReadMeta(namespace []byte) ([]byte, error) {
-	return nil, nil
-}
