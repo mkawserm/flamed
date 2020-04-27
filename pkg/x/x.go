73282f1148225a0c27416bc451008d3ed58befb1
kawser <mkawserm@gmail.com>
2020-04-27 12:47:46 +0600

    ERRORS ADDED

---
+++ pkg/x/x.go
@@ -0,0 +1,69 @@
+package x
+
+import "errors"
+
+var ErrFailedToOpenStorage = errors.New("failed to open the storage")
+var ErrFailedToCloseStorage = errors.New("failed to close the storage")
+var ErrFailedToChangeSecretKey = errors.New("failed to change secret key")
+var ErrFailedToReadDataFromStorage = errors.New("failed to read data from the storage")
+var ErrFailedToDeleteDataFromStorage = errors.New("failed to delete data from the storage")
+var ErrFailedToCreateDataToStorage = errors.New("failed to create data to the storage")
+var ErrFailedToUpdateDataToStorage = errors.New("failed to update data to the storage")
+var ErrFailedToApplyBatchToStorage = errors.New("failed to apply batch to the storage")
+var ErrFailedToApplyActionToStorage = errors.New("failed to apply action to the storage")
+var ErrFailedToGenerateAsyncSnapshotFromStorage = errors.New("failed to generate async snapshot from the storage")
+var ErrFailedToApplyAsyncSnapshotToStorage = errors.New("failed to apply async snapshot to the storage")
+var ErrFailedToGenerateSyncSnapshotFromStorage = errors.New("failed to generate sync snapshot from the storage")
+var ErrFailedToApplySyncSnapshotToStorage = errors.New("failed to apply sync snapshot to the storage")
+
+func IsFailedToOpenStorage(err error) bool {
+	return err == ErrFailedToOpenStorage
+}
+
+func IsFailedToCloseStorage(err error) bool {
+	return err == ErrFailedToCloseStorage
+}
+
+func IsFailedToChangeSecretKey(err error) bool {
+	return err == ErrFailedToChangeSecretKey
+}
+
+func IsFailedToReadDataFromStorage(err error) bool {
+	return err == ErrFailedToReadDataFromStorage
+}
+
+func IsFailedToDeleteDataFromStorage(err error) bool {
+	return err == ErrFailedToDeleteDataFromStorage
+}
+
+func IsFailedToCreateDataToStorage(err error) bool {
+	return err == ErrFailedToCreateDataToStorage
+}
+
+func IsFailedToUpdateDataToStorage(err error) bool {
+	return err == ErrFailedToUpdateDataToStorage
+}
+
+func IsFailedToApplyBatchToStorage(err error) bool {
+	return err == ErrFailedToApplyBatchToStorage
+}
+
+func IsFailedToApplyActionToStorage(err error) bool {
+	return err == ErrFailedToApplyActionToStorage
+}
+
+func IsFailedToGenerateAsyncSnapshotFromStorage(err error) bool {
+	return err == ErrFailedToGenerateAsyncSnapshotFromStorage
+}
+
+func IsFailedToApplyAsyncSnapshotToStorage(err error) bool {
+	return err == ErrFailedToApplyAsyncSnapshotToStorage
+}
+
+func IsFailedToGenerateSyncSnapshotFromStorage(err error) bool {
+	return err == ErrFailedToGenerateSyncSnapshotFromStorage
+}
+
+func IsFailedToApplySyncSnapshotToStorage(err error) bool {
+	return err == ErrFailedToApplySyncSnapshotToStorage
+}
---
+++ pkg/x/x_test.go
@@ -0,0 +1,106 @@
+package x
+
+import "testing"
+
+func TestIsFailedToApplyActionToStorage(t *testing.T) {
+	t.Helper()
+	if !IsFailedToApplyActionToStorage(ErrFailedToApplyActionToStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToApplyAsyncSnapshotToStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToApplyAsyncSnapshotToStorage(ErrFailedToApplyAsyncSnapshotToStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToApplyBatchToStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToApplyBatchToStorage(ErrFailedToApplyBatchToStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToApplySyncSnapshotToStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToApplySyncSnapshotToStorage(ErrFailedToApplySyncSnapshotToStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToChangeSecretKey(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToChangeSecretKey(ErrFailedToChangeSecretKey) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToCloseStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToCloseStorage(ErrFailedToCloseStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToCreateDataToStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToCreateDataToStorage(ErrFailedToCreateDataToStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToDeleteDataFromStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToDeleteDataFromStorage(ErrFailedToDeleteDataFromStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToGenerateAsyncSnapshotFromStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToGenerateAsyncSnapshotFromStorage(ErrFailedToGenerateAsyncSnapshotFromStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToGenerateSyncSnapshotFromStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToGenerateSyncSnapshotFromStorage(ErrFailedToGenerateSyncSnapshotFromStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToOpenStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToOpenStorage(ErrFailedToOpenStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToReadDataFromStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToReadDataFromStorage(ErrFailedToReadDataFromStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
+
+func TestIsFailedToUpdateDataToStorage(t *testing.T) {
+	t.Helper()
+
+	if !IsFailedToUpdateDataToStorage(ErrFailedToUpdateDataToStorage) {
+		t.Fatalf("error mismatch")
+	}
+}
