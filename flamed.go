4d3ecb666444dc542c2a1e65ee95383753228a15
kawser <mkawserm@gmail.com>
2020-04-28 06:21:28 +0600

    REFACTORED

--- flamed.go
+++ flamed.go
@@ -4,5 +4,5 @@ import internalStorage "github.com/mkawserm/flamed/pkg/storage"
 import internalStoraged "github.com/mkawserm/flamed/pkg/storaged"
 
 type Storage internalStorage.Storage
-type Storaged internalStoraged.Storaged
 type Cluster internalStoraged.Cluster
+type Storaged internalStoraged.Storaged
