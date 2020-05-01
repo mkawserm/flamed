9e0c9e102617b22296b23a578177451abb6de7a8
kawser <mkawserm@gmail.com>
2020-05-02 03:11:24 +0600

    VALIDATOR UPDATED

--- pkg/utility/validator.go
+++ pkg/utility/validator.go
@@ -1,1 +1,13 @@
 package utility
+
+func IsNamespaceValid(namespace []byte) bool {
+	if len(namespace) < 3 {
+		return false
+	}
+
+	if namespace[0] >= 'A' && namespace[1] <= 'z' {
+		return true
+	}
+
+	return false
+}
