7a95ea6b5d3c38a682e572ad571fd68393b8851f
kawser <mkawserm@gmail.com>
2020-05-04 07:50:24 +0600

    MINOR REFACTORING

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -280,10 +280,11 @@ func (s *Storage) ApplyProposal(pp *pb.FlameProposal, checkNamespaceValidity boo
 				}
 			}
 		}
-
+		err := s.applyBatchAction(batchAction)
+		if err != nil {
+			return err
+		}
 		_ = s.directIndex(batchAction)
-
-		return s.applyBatchAction(batchAction)
 	} else if pp.FlameProposalType == pb.FlameProposal_CREATE_INDEX_META {
 		indexMeta := &pb.FlameIndexMeta{}
 		if err := proto.Unmarshal(pp.FlameProposalData, indexMeta); err != nil {
@@ -299,11 +300,12 @@ func (s *Storage) ApplyProposal(pp *pb.FlameProposal, checkNamespaceValidity boo
 		if err := s.createIndexMeta(indexMeta); err != nil {
 			if err := s.mIndexStorage.CreateIndexMeta(indexMeta); err != nil {
 				internalLogger.Error("CreateIndexMeta error", zap.Error(err))
+			} else {
+				return x.ErrFailedToApplyProposal
 			}
 		} else {
-			return err
+			return nil
 		}
-
 	} else if pp.FlameProposalType == pb.FlameProposal_UPDATE_INDEX_META {
 		indexMeta := &pb.FlameIndexMeta{}
 		if err := proto.Unmarshal(pp.FlameProposalData, indexMeta); err != nil {
@@ -319,9 +321,11 @@ func (s *Storage) ApplyProposal(pp *pb.FlameProposal, checkNamespaceValidity boo
 		if err := s.updateIndexMeta(indexMeta); err != nil {
 			if err := s.mIndexStorage.UpdateIndexMeta(indexMeta); err != nil {
 				internalLogger.Error("UpdateIndexMeta error", zap.Error(err))
+			} else {
+				return x.ErrFailedToApplyProposal
 			}
 		} else {
-			return err
+			return nil
 		}
 	} else if pp.FlameProposalType == pb.FlameProposal_DELETE_INDEX_META {
 		indexMeta := &pb.FlameIndexMeta{}
@@ -338,9 +342,11 @@ func (s *Storage) ApplyProposal(pp *pb.FlameProposal, checkNamespaceValidity boo
 		if err := s.deleteIndexMeta(indexMeta); err != nil {
 			if err := s.mIndexStorage.DeleteIndexMeta(indexMeta); err != nil {
 				internalLogger.Error("DeleteIndexMeta error", zap.Error(err))
+			} else {
+				return x.ErrFailedToApplyProposal
 			}
 		} else {
-			return err
+			return nil
 		}
 	} else if pp.FlameProposalType == pb.FlameProposal_CREATE_ACCESS_CONTROL {
 		ac := &pb.FlameAccessControl{}
