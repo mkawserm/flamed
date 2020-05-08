9045ec43b3b2bf13411433eacfa95b959077e759
kawser <mkawserm@gmail.com>
2020-05-09 01:40:18 +0600

    ODM UPDATED

--- pkg/odm/odm.go
+++ pkg/odm/odm.go
@@ -3,7 +3,5 @@ package odm
 import "github.com/mkawserm/flamed/pkg/iface"
 
 type ODM struct {
-	QueryObject       iface.IQuery
-	MutationObject    iface.IMutation
 	TransformerObject iface.ITransformer
 }
