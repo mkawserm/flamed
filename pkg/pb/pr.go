4495fc204a4f925de03be4d59cbd1f528e1388ba
kawser <mkawserm@gmail.com>
2020-05-14 00:37:51 +0600

    PROTO UPDATED

--- pkg/pb/pr.go
+++ pkg/pb/pr.go
@@ -1,14 +1,14 @@
 package pb
 
 func (m *ProposalResponse) Append(tpr *TransactionProcessorResponse) {
-	if m.TransactionProcessorResponseList == nil {
-		m.TransactionProcessorResponseList = make([]*TransactionProcessorResponse, 0)
+	if m.TransactionProcessorResponses == nil {
+		m.TransactionProcessorResponses = make([]*TransactionProcessorResponse, 0)
 	}
-	m.TransactionProcessorResponseList = append(m.TransactionProcessorResponseList, tpr)
+	m.TransactionProcessorResponses = append(m.TransactionProcessorResponses, tpr)
 }
 
 func NewProposalResponse(status uint32) *ProposalResponse {
 	return &ProposalResponse{
-		Status:                           status,
-		TransactionProcessorResponseList: make([]*TransactionProcessorResponse, 0)}
+		Status:                        status,
+		TransactionProcessorResponses: make([]*TransactionProcessorResponse, 0)}
 }
