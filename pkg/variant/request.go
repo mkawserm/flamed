216f2b252a16e30e6735bcef3fb12874602293bb
kawser <mkawserm@gmail.com>
2020-05-15 01:12:16 +0600

    REQUEST UPDATED

--- pkg/variant/request.go
+++ pkg/variant/request.go
@@ -3,7 +3,7 @@ package variant
 import "context"
 
 type LookupRequest struct {
-	Query   interface{}     `json:"-"`
+	Query   interface{}     `json:"query"`
 	Context context.Context `json:"-"`
 
 	FamilyName    string `json:"familyName"`
@@ -11,7 +11,7 @@ type LookupRequest struct {
 }
 
 type SearchRequest struct {
-	Query     interface{}     `json:"-"`
+	Query     interface{}     `json:"query"`
 	Context   context.Context `json:"-"`
 	Namespace []byte          `json:"namespace"`
 }
