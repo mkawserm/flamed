0e1b24cc3d77508c8b4baa136506366d23cd5b12
kawser <mkawserm@gmail.com>
2020-05-15 12:22:47 +0600

    OLD CODE REMOVED

--- pkg/storage/iterator.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-//type Iterator struct {
-//	Seek     []byte                          `json:"seek"`
-//	Prefix   []byte                          `json:"prefix"`
-//	Limit    int                             `json:"limit"`
-//	Receiver func(entry *pb.FlameEntry) bool `json:"-"`
-//}
