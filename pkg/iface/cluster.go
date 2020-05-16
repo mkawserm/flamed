12ca633231176346a5d6ef5bebc03cf66f6900a3
kawser <mkawserm@gmail.com>
2020-05-16 22:50:18 +0600

    INTERFACE UPDATED

--- pkg/iface/cluster.go
+++ pkg/iface/cluster.go
@@ -3,9 +3,9 @@ package iface
 import sm "github.com/lni/dragonboat/v3/statemachine"
 
 type IOnDiskClusterConfiguration interface {
+	Join() bool
 	ClusterID() uint64
 	ClusterName() string
 	InitialMembers() map[uint64]string
-	Join() bool
 	StateMachine(IStoragedConfiguration) func(uint64, uint64) sm.IOnDiskStateMachine
 }
