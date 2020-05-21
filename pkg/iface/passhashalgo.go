038c91dc99bf350c2e0688698caec5f914a0f420
kawser <mkawserm@gmail.com>
2020-05-21 11:51:38 +0600

    INTERFACE UPDATED

--- pkg/iface/passhashalgo.go
+++ pkg/iface/passhashalgo.go
@@ -4,3 +4,10 @@ type PasswordHashAlgorithm interface {
 	Encode(password string, salt string) (string, error)
 	Verify(password string, encoded string) (bool, error)
 }
+
+type PasswordHashAlgorithmFactory interface {
+	AppendPasswordHashAlgorithm(ph PasswordHashAlgorithm)
+
+	CheckPassword(password, encoded string) (bool, error)
+	MakePassword(password, salt, algorithm string) (string, error)
+}
