a9fade24ff33b940a551cc25bb95093761b2ac92
kawser <mkawserm@gmail.com>
2020-05-22 00:59:59 +0600

    WORKING ON VIPER BASED CONFIGURATION

--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	github.com/golang/protobuf v1.4.1
 	github.com/google/uuid v1.1.1
 	github.com/lni/dragonboat/v3 v3.1.1-0.20200518120809-5ce914b80953
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.7.0
 	go.uber.org/zap v1.15.0
 	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
 )
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,22 +1,27 @@
 package app
 
 import (
+	"fmt"
 	"github.com/mkawserm/flamed/pkg/iface"
 	"github.com/mkawserm/flamed/pkg/tp/accesscontrol"
 	"github.com/mkawserm/flamed/pkg/tp/indexmeta"
 	"github.com/mkawserm/flamed/pkg/tp/intkey"
 	"github.com/mkawserm/flamed/pkg/tp/json"
 	"github.com/mkawserm/flamed/pkg/tp/user"
+	"os"
 	"sync"
 	"time"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/mkawserm/flamed/pkg/flamed"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
-	appOnce sync.Once
-	appIns  *App
+	appOnce    sync.Once
+	appIns     *App
+	configFile string
 )
 
 type App struct {
@@ -55,9 +60,18 @@ func (a *App) setup() {
 		Use:   "flamed",
 		Short: "Flamed is an open-source distributed embeddable NoSQL database",
 		Long:  "Flamed is an open-source distributed embeddable NoSQL database",
-		Run: func(_ *cobra.Command, _ []string) {
+		Run: func(cmd *cobra.Command, _ []string) {
+			fmt.Println(cmd.UsageString())
 		},
 	}
+
+	a.mRootCommand.
+		PersistentFlags().
+		StringVar(
+			&configFile,
+			"config",
+			"",
+			"config file (default is $HOME/flamed/.flamed.yaml)")
 
 	a.mRootCommand.AddCommand(runCMD)
 	a.mRootCommand.AddCommand(authorCMD)
@@ -72,7 +86,73 @@ func GetApp() *App {
 	return appIns
 }
 
+func initConfig() {
+	// Find home directory.
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	flamedHomePath := home + "/flamed"
+
+	if configFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(configFile)
+	} else {
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(flamedHomePath)
+		viper.SetConfigName(".flamed")
+	}
+
+	viper.SetEnvPrefix("flamed")
+	viper.AutomaticEnv()
+
+	// SET DEFAULTS
+	viper.SetDefault("StoragePath", flamedHomePath)
+	viper.SetDefault("RaftAddress", "")
+	viper.SetDefault("HTTPAddress", "")
+	viper.SetDefault("Join", false)
+	viper.SetDefault("InitialMembers", "")
+
+	viper.SetDefault("HTTPServerTLS", false)
+	viper.SetDefault("HTTPServerCertFile", "")
+	viper.SetDefault("HTTPServerKeyFile", "")
+
+	viper.SetDefault("DeploymentID", 1)
+	viper.SetDefault("RTTMillisecond", 200)
+	viper.SetDefault("MutualTLS", false)
+	viper.SetDefault("CAFile", "")
+	viper.SetDefault("CertFile", "")
+	viper.SetDefault("KeyFile", "")
+	viper.SetDefault("MaxSendQueueSize", 0)
+	viper.SetDefault("MaxReceiveQueueSize", 0)
+	viper.SetDefault("EnableMetrics", false)
+	viper.SetDefault("MaxSnapshotSendBytesPerSecond", 0)
+	viper.SetDefault("MaxSnapshotRecvBytesPerSecond", 0)
+	viper.SetDefault("NotifyCommit", false)
+	viper.SetDefault("LogDBConfig", "tiny")
+
+	viper.SetDefault("NodeID", 1)
+	viper.SetDefault("CheckQuorum", true)
+	viper.SetDefault("ElectionRTT", 5)
+	viper.SetDefault("HeartbeatRTT", 1)
+	viper.SetDefault("SnapshotEntries", 100)
+	viper.SetDefault("CompactionOverhead", 5)
+	viper.SetDefault("OrderedConfigChange", false)
+	viper.SetDefault("MaxInMemLogSize", 0)
+	viper.SetDefault("DisableAutoCompactions", false)
+	viper.SetDefault("IsObserver", false)
+	viper.SetDefault("IsWitness", false)
+	viper.SetDefault("Quiesce", false)
+
+	if err := viper.ReadInConfig(); err == nil {
+		//fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
 func init() {
+	cobra.OnInitialize(initConfig)
+
 	appOnce.Do(func() {
 		appIns = &App{}
 		appIns.setup()
