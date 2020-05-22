d3037f2e3d7329b400c72a852f7676162caf1aee
kawser <mkawserm@gmail.com>
2020-05-22 23:30:47 +0600

    MINOR REFACTORING

--- pkg/app/run.go
+++ pkg/app/run.go
@@ -25,7 +25,6 @@ var RunCMD = &cobra.Command{
 	Use:   "run",
 	Short: "Run Flamed server",
 	Run: func(cmd *cobra.Command, args []string) {
-		logger.GetLoggerFactory().ChangeLogLevel(viper.GetString("LogLevel"))
 		if len(viper.GetString(StoragePath)) == 0 {
 			fmt.Println("StoragePath can not be empty")
 			return
@@ -41,103 +40,26 @@ var RunCMD = &cobra.Command{
 			return
 		}
 
-		im := getInitialMembers(strings.Split(viper.GetString(InitialMembers), ";"))
-
-		if len(im) == 0 {
-			if !viper.GetBool("Join") {
-				im[viper.GetUint64(NodeID)] = viper.GetString(RaftAddress)
-			}
-		}
-
 		raftStoragePath := viper.GetString(StoragePath) + "/raft"
-
-		nodeConfiguration := &conf.NodeConfiguration{
-			NodeConfigurationInput: conf.NodeConfigurationInput{
-				NodeHostDir:                   raftStoragePath,
-				WALDir:                        raftStoragePath,
-				DeploymentID:                  viper.GetUint64(DeploymentID),
-				RTTMillisecond:                viper.GetUint64(RTTMillisecond),
-				RaftAddress:                   viper.GetString(RaftAddress),
-				ListenAddress:                 "",
-				MutualTLS:                     viper.GetBool(MutualTLS),
-				CAFile:                        viper.GetString(CAFile),
-				CertFile:                      viper.GetString(CertFile),
-				KeyFile:                       viper.GetString(KeyFile),
-				MaxSendQueueSize:              viper.GetUint64(MaxSendQueueSize),
-				MaxReceiveQueueSize:           viper.GetUint64(MaxReceiveQueueSize),
-				EnableMetrics:                 viper.GetBool(EnableMetrics),
-				MaxSnapshotSendBytesPerSecond: viper.GetUint64(MaxSnapshotSendBytesPerSecond),
-				MaxSnapshotRecvBytesPerSecond: viper.GetUint64(MaxSnapshotRecvBytesPerSecond),
-				NotifyCommit:                  viper.GetBool(NotifyCommit),
-
-				SystemTickerPrecision: viper.GetDuration(SystemTickerPrecision),
-
-				LogDBConfig:         config.GetTinyMemLogDBConfig(),
-				LogDBFactory:        nil,
-				RaftRPCFactory:      nil,
-				RaftEventListener:   nil,
-				SystemEventListener: nil,
-			},
-		}
-
+		// node configuration
+		nodeConfiguration := getNodeConfiguration(raftStoragePath)
 		if viper.GetString("LogDBConfig") == "tiny" {
 			nodeConfiguration.NodeConfigurationInput.LogDBConfig = config.GetTinyMemLogDBConfig()
 		} else {
 			nodeConfiguration.NodeConfigurationInput.LogDBConfig = config.GetTinyMemLogDBConfig()
 		}
-
 		err := GetApp().GetFlamed().ConfigureNode(nodeConfiguration)
-
 		if err != nil {
 			panic(err)
 		}
 
-		clusterStoragePath := viper.GetString(StoragePath) + "/cluster-1"
-
-		storagedConfiguration := conf.SimpleStoragedConfiguration(clusterStoragePath, nil)
-		for _, tp := range GetApp().mTransactionProcessorList {
-			storagedConfiguration.AddTransactionProcessor(tp)
-		}
-
-		clusterConfiguration := conf.SimpleOnDiskClusterConfiguration(
-			1,
-			"cluster-1",
-			im,
-			viper.GetBool(Join))
-
-		raftConfiguration := &conf.RaftConfiguration{
-			RaftConfigurationInput: conf.RaftConfigurationInput{
-				NodeID:                 viper.GetUint64(NodeID),
-				CheckQuorum:            viper.GetBool(CheckQuorum),
-				ElectionRTT:            viper.GetUint64(ElectionRTT),
-				HeartbeatRTT:           viper.GetUint64(HeartbeatRTT),
-				SnapshotEntries:        viper.GetUint64(SnapshotEntries),
-				CompactionOverhead:     viper.GetUint64(CompactionOverhead),
-				OrderedConfigChange:    viper.GetBool(OrderedConfigChange),
-				MaxInMemLogSize:        viper.GetUint64(MaxInMemLogSize),
-				DisableAutoCompactions: viper.GetBool(DisableAutoCompactions),
-				IsObserver:             viper.GetBool(IsObserver),
-				IsWitness:              viper.GetBool(IsWitness),
-				Quiesce:                viper.GetBool(Quiesce),
-			},
-		}
-
-		err = GetApp().GetFlamed().StartOnDiskCluster(
-			clusterConfiguration,
-			storagedConfiguration,
-			raftConfiguration)
-
-		if err != nil {
-			panic(err)
-		}
-
+		// start cluster
+		startCluster()
 		// initialize cluster defaults
 		// like admin user and other things
 		initializeClusterDefaults()
-
 		// initialize views
 		GetApp().initViews()
-
 		// run server and wait for shutdown
 		runServerAndWaitForShutdown()
 	},
@@ -304,5 +226,89 @@ func initializeClusterDefaults() {
 		logger.L("app").Info("admin user created")
 	} else {
 		logger.L("app").Error("failed to create admin user")
+	}
+}
+
+func getNodeConfiguration(raftStoragePath string) *conf.NodeConfiguration {
+	return &conf.NodeConfiguration{
+		NodeConfigurationInput: conf.NodeConfigurationInput{
+			NodeHostDir:                   raftStoragePath,
+			WALDir:                        raftStoragePath,
+			DeploymentID:                  viper.GetUint64(DeploymentID),
+			RTTMillisecond:                viper.GetUint64(RTTMillisecond),
+			RaftAddress:                   viper.GetString(RaftAddress),
+			ListenAddress:                 "",
+			MutualTLS:                     viper.GetBool(MutualTLS),
+			CAFile:                        viper.GetString(CAFile),
+			CertFile:                      viper.GetString(CertFile),
+			KeyFile:                       viper.GetString(KeyFile),
+			MaxSendQueueSize:              viper.GetUint64(MaxSendQueueSize),
+			MaxReceiveQueueSize:           viper.GetUint64(MaxReceiveQueueSize),
+			EnableMetrics:                 viper.GetBool(EnableMetrics),
+			MaxSnapshotSendBytesPerSecond: viper.GetUint64(MaxSnapshotSendBytesPerSecond),
+			MaxSnapshotRecvBytesPerSecond: viper.GetUint64(MaxSnapshotRecvBytesPerSecond),
+			NotifyCommit:                  viper.GetBool(NotifyCommit),
+
+			SystemTickerPrecision: viper.GetDuration(SystemTickerPrecision),
+
+			LogDBConfig:         config.GetTinyMemLogDBConfig(),
+			LogDBFactory:        nil,
+			RaftRPCFactory:      nil,
+			RaftEventListener:   nil,
+			SystemEventListener: nil,
+		},
+	}
+}
+
+func getRaftConfiguration() *conf.RaftConfiguration {
+	return &conf.RaftConfiguration{
+		RaftConfigurationInput: conf.RaftConfigurationInput{
+			NodeID:                 viper.GetUint64(NodeID),
+			CheckQuorum:            viper.GetBool(CheckQuorum),
+			ElectionRTT:            viper.GetUint64(ElectionRTT),
+			HeartbeatRTT:           viper.GetUint64(HeartbeatRTT),
+			SnapshotEntries:        viper.GetUint64(SnapshotEntries),
+			CompactionOverhead:     viper.GetUint64(CompactionOverhead),
+			OrderedConfigChange:    viper.GetBool(OrderedConfigChange),
+			MaxInMemLogSize:        viper.GetUint64(MaxInMemLogSize),
+			DisableAutoCompactions: viper.GetBool(DisableAutoCompactions),
+			IsObserver:             viper.GetBool(IsObserver),
+			IsWitness:              viper.GetBool(IsWitness),
+			Quiesce:                viper.GetBool(Quiesce),
+		},
+	}
+}
+
+func startCluster() {
+	im := getInitialMembers(strings.Split(viper.GetString(InitialMembers), ";"))
+
+	if len(im) == 0 {
+		if !viper.GetBool("Join") {
+			im[viper.GetUint64(NodeID)] = viper.GetString(RaftAddress)
+		}
+	}
+
+	clusterStoragePath := viper.GetString(StoragePath) + "/cluster-1"
+
+	storagedConfiguration := conf.SimpleStoragedConfiguration(clusterStoragePath, nil)
+	for _, tp := range GetApp().mTransactionProcessorList {
+		storagedConfiguration.AddTransactionProcessor(tp)
+	}
+
+	clusterConfiguration := conf.SimpleOnDiskClusterConfiguration(
+		1,
+		"cluster-1",
+		im,
+		viper.GetBool(Join))
+
+	raftConfiguration := getRaftConfiguration()
+
+	err := GetApp().GetFlamed().StartOnDiskCluster(
+		clusterConfiguration,
+		storagedConfiguration,
+		raftConfiguration)
+
+	if err != nil {
+		panic(err)
 	}
 }
