9fe97cfa0a00fad3a3081879b3a09a9bb6d594f7
kawser <mkawserm@gmail.com>
2020-05-23 18:20:55 +0600

    IS LIVE REFACTORED

--- pkg/app/view/graphql/query/islive.go
+++ pkg/app/view/graphql/query/islive.go
@@ -5,13 +5,62 @@ import (
 	"github.com/mkawserm/flamed/pkg/context"
 )
 
+type ServiceStatus struct {
+	HTTPServer bool
+	GRPCServer bool
+	RAFTServer bool
+}
+
+var ServiceStatusType = graphql.NewObject(graphql.ObjectConfig{
+	Name:        "ServiceStatus",
+	Description: "`ServiceStatus` provides information about service availability",
+	Fields: graphql.Fields{
+		"httpServer": &graphql.Field{
+			Type:        graphql.Boolean,
+			Description: "Is HTTP server available?",
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				if val, ok := p.Source.(ServiceStatus); ok {
+					return val.HTTPServer, nil
+				}
+				return nil, nil
+			},
+		},
+
+		"grpcServer": &graphql.Field{
+			Type:        graphql.Boolean,
+			Description: "Is GRPC server available?",
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				if val, ok := p.Source.(ServiceStatus); ok {
+					return val.GRPCServer, nil
+				}
+				return nil, nil
+			},
+		},
+
+		"raftServer": &graphql.Field{
+			Type:        graphql.Boolean,
+			Description: "Is RAFT server available?",
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				if val, ok := p.Source.(ServiceStatus); ok {
+					return val.RAFTServer, nil
+				}
+				return nil, nil
+			},
+		},
+	},
+})
+
 func IsLive(_ *context.FlamedContext) *graphql.Field {
 	return &graphql.Field{
 		Name: "IsLive",
-		Type: graphql.Boolean,
+		Type: ServiceStatusType,
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-			return true, nil
+			return ServiceStatus{
+				HTTPServer: true,
+				GRPCServer: false,
+				RAFTServer: true,
+			}, nil
 		},
-		Description: "Service availability status",
+		Description: "Query about service availability information",
 	}
 }
