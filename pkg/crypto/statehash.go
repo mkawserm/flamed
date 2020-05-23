5b0d8b82ba911117a09cfca473afc12ce4c5cdbf
kawser <mkawserm@gmail.com>
2020-05-23 11:06:02 +0600

    NEW STATE HASH METHOD ADDED

--- pkg/crypto/statehash.go
+++ pkg/crypto/statehash.go
@@ -14,3 +14,15 @@ func GetStateHashFromStringKey(familyName, key string) []byte {
 	copy(r[3:], keyHash[32:])
 	return r
 }
+
+// GetStateHashFromUint64Key generate 35 bytes length
+// state hash from uint64 based key
+func GetStateHashFromUint64Key(familyName string, key uint64) []byte {
+	var newKeyBytes = Uint64ToByteSlice(key)
+	familyNameHash := sha512.Sum512([]byte(familyName))
+	keyHash := sha512.Sum512(newKeyBytes)
+	r := make([]byte, 35)
+	r = familyNameHash[0:3]
+	copy(r[3:], keyHash[32:])
+	return r
+}
