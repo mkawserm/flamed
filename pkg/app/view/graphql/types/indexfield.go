cac2e8136d2ee447c64f39d1d554fad79bd918c5
kawser <mkawserm@gmail.com>
2020-05-26 00:35:43 +0600

    FILE SEPARATED

---
+++ pkg/app/view/graphql/types/indexfield.go
@@ -0,0 +1,129 @@
+package types
+
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/mkawserm/flamed/pkg/pb"
+)
+
+var IndexFieldType = graphql.NewObject(graphql.ObjectConfig{
+	Name:        "IndexField",
+	Description: "`IndexField`",
+	Fields: graphql.Fields{
+		"indexFieldType": &graphql.Field{
+			Name:        "IndexFieldType",
+			Description: "",
+			Type:        IndexFieldTypeEnum,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return int(indexField.IndexFieldType), nil
+			},
+		},
+
+		"name": &graphql.Field{
+			Name:        "Name",
+			Description: "",
+			Type:        graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return indexField.Name, nil
+			},
+		},
+
+		"analyzer": &graphql.Field{
+			Name:        "Analyzer",
+			Description: "",
+			Type:        graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return indexField.Analyzer, nil
+			},
+		},
+
+		"enabled": &graphql.Field{
+			Name:        "Enabled",
+			Description: "",
+			Type:        graphql.Boolean,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return indexField.Enabled, nil
+			},
+		},
+
+		"index": &graphql.Field{
+			Name:        "Index",
+			Description: "",
+			Type:        graphql.Boolean,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return indexField.Index, nil
+			},
+		},
+
+		"store": &graphql.Field{
+			Name:        "Store",
+			Description: "",
+			Type:        graphql.Boolean,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return indexField.Store, nil
+			},
+		},
+
+		"includeTermVectors": &graphql.Field{
+			Name:        "IncludeTermVectors",
+			Description: "",
+			Type:        graphql.Boolean,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return indexField.IncludeTermVectors, nil
+			},
+		},
+
+		"includeInAll": &graphql.Field{
+			Name:        "IncludeInAll",
+			Description: "",
+			Type:        graphql.Boolean,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return indexField.IncludeInAll, nil
+			},
+		},
+
+		"docValues": &graphql.Field{
+			Name:        "DocValues",
+			Description: "",
+			Type:        graphql.Boolean,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				indexField, ok := p.Source.(*pb.IndexField)
+				if !ok {
+					return nil, nil
+				}
+				return indexField.DocValues, nil
+			},
+		},
+	},
+})
---
+++ pkg/app/view/graphql/types/indexfieldtype.go
@@ -0,0 +1,34 @@
+package types
+
+import "github.com/graphql-go/graphql"
+
+var IndexFieldTypeEnum = graphql.NewEnum(graphql.EnumConfig{
+	Name:        "IndexFieldType",
+	Description: "`IndexFieldType` defines enum for `IndexField`",
+	Values: graphql.EnumValueConfigMap{
+		"TEXT": &graphql.EnumValueConfig{
+			Value:       0,
+			Description: "TEXT",
+		},
+
+		"NUMERIC": &graphql.EnumValueConfig{
+			Value:       1,
+			Description: "NUMERIC",
+		},
+
+		"BOOLEAN": &graphql.EnumValueConfig{
+			Value:       2,
+			Description: "BOOLEAN",
+		},
+
+		"GEO_POINT": &graphql.EnumValueConfig{
+			Value:       3,
+			Description: "GEO_POINT",
+		},
+
+		"DATE_TIME": &graphql.EnumValueConfig{
+			Value:       4,
+			Description: "DATE_TIME",
+		},
+	},
+})
