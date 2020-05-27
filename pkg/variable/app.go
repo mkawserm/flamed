2c18816946a2252e4d4fadfa01de97841b7dd871
kawser <mkawserm@gmail.com>
2020-05-27 12:56:39 +0600

    PROJECT REFACTORED

--- pkg/variable/app.go
+++ pkg/variable/app.go
@@ -1,9 +1,12 @@
 package variable
 
-import "github.com/mkawserm/flamed/pkg/iface"
+import (
+	"github.com/mkawserm/flamed/pkg/crypto"
+	"github.com/mkawserm/flamed/pkg/iface"
+)
 
 var Name = "flamed"
 var ShortDescription = "mFlamed is an open-source distributed embeddable NoSQL database"
 var LongDescription = "mFlamed is an open-source distributed embeddable NoSQL database"
 var DefaultPasswordHashAlgorithm = "argon2"
-var DefaultPasswordHashAlgorithmFactory iface.IPasswordHashAlgorithmFactory
+var DefaultPasswordHashAlgorithmFactory iface.IPasswordHashAlgorithmFactory = crypto.DefaultPasswordHashAlgorithmFactory()
