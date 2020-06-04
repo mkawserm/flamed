94ed0daa9ec0af425f3ed68198ecf5f19ca266ba
kawser <mkawserm@gmail.com>
2020-06-04 07:59:00 +0600

    SEARCH RESPONSE INTERFACE UPDATED

--- pkg/iface/searchresponse.go
+++ pkg/iface/searchresponse.go
@@ -2,6 +2,36 @@ package iface
 
 import "time"
 
+type ITerm interface {
+	Term() string
+	Count() int
+}
+
+type IDateRangeFacet interface {
+	Name() string
+	Start() string
+	End() string
+	Count() int
+}
+
+type INumericRangeFacet interface {
+	Name() string
+	Min() float64
+	Max() float64
+	Count() int
+}
+
+type IFacet interface {
+	Name() string
+	Field() string
+	Total() int
+	Missing() int
+	Other() int
+	Terms() []ITerm
+	NumericRanges() []INumericRangeFacet
+	DateRanges() []IDateRangeFacet
+}
+
 type IDocument interface {
 	ID() string
 	Score() float64
@@ -13,4 +43,5 @@ type ISearchResponse interface {
 	MaxScore() float64
 	Took() time.Duration
 	Hits() []IDocument
+	Facets() []IFacet
 }
--- pkg/plugin/storage/index/blevescorch/searchresult.go
+++ pkg/plugin/storage/index/blevescorch/searchresult.go
@@ -9,6 +9,124 @@ import (
 	"time"
 )
 
+type BleveDateRangeFacet struct {
+	mDRF *search.DateRangeFacet
+}
+
+func (b *BleveDateRangeFacet) Name() string {
+	return b.mDRF.Name
+}
+
+func (b *BleveDateRangeFacet) Start() string {
+	var s = *b.mDRF.Start
+	return s
+}
+
+func (b *BleveDateRangeFacet) End() string {
+	var s = *b.mDRF.End
+	return s
+}
+
+func (b *BleveDateRangeFacet) Count() int {
+	return b.mDRF.Count
+}
+
+type BleveNumericRangeFacet struct {
+	mNRF *search.NumericRangeFacet
+}
+
+func (b *BleveNumericRangeFacet) Name() string {
+	return b.mNRF.Name
+}
+
+func (b *BleveNumericRangeFacet) Min() float64 {
+	return *b.mNRF.Min
+}
+
+func (b *BleveNumericRangeFacet) Max() float64 {
+	return *b.mNRF.Max
+}
+
+func (b *BleveNumericRangeFacet) Count() int {
+	return b.mNRF.Count
+}
+
+type BleveTerm struct {
+	mTerm *search.TermFacet
+}
+
+func (b *BleveTerm) Term() string {
+	return b.mTerm.Term
+}
+
+func (b *BleveTerm) Count() int {
+	return b.mTerm.Count
+}
+
+type BleveFacet struct {
+	mName        string
+	mFacetResult *search.FacetResult
+}
+
+func (b *BleveFacet) Name() string {
+	return b.mName
+}
+
+func (b *BleveFacet) Field() string {
+	return b.mFacetResult.Field
+}
+
+func (b *BleveFacet) Total() int {
+	return b.mFacetResult.Total
+}
+
+func (b *BleveFacet) Missing() int {
+	return b.mFacetResult.Missing
+}
+
+func (b *BleveFacet) Other() int {
+	return b.mFacetResult.Other
+}
+
+func (b *BleveFacet) Terms() []iface.ITerm {
+	if len(b.mFacetResult.Terms) == 0 {
+		return nil
+	}
+
+	terms := make([]iface.ITerm, 0, len(b.mFacetResult.Terms))
+	for _, doc := range b.mFacetResult.Terms {
+		terms = append(terms, &BleveTerm{mTerm: doc})
+	}
+
+	return terms
+}
+
+func (b *BleveFacet) NumericRanges() []iface.INumericRangeFacet {
+	if len(b.mFacetResult.NumericRanges) == 0 {
+		return nil
+	}
+
+	numericRangeFacets := make([]iface.INumericRangeFacet, 0, len(b.mFacetResult.NumericRanges))
+	for _, doc := range b.mFacetResult.NumericRanges {
+		numericRangeFacets = append(numericRangeFacets, &BleveNumericRangeFacet{mNRF: doc})
+	}
+
+	return numericRangeFacets
+}
+
+func (b *BleveFacet) DateRanges() []iface.IDateRangeFacet {
+	if len(b.mFacetResult.DateRanges) == 0 {
+		return nil
+	}
+
+	dateRangeFacets := make([]iface.IDateRangeFacet, 0, len(b.mFacetResult.DateRanges))
+	for _, doc := range b.mFacetResult.DateRanges {
+		dateRangeFacets = append(dateRangeFacets, &BleveDateRangeFacet{mDRF: doc})
+	}
+
+	return dateRangeFacets
+}
+
 type BleveSearchDocument struct {
 	DocMatch *search.DocumentMatch
 }
@@ -26,8 +144,7 @@ func (b *BleveSearchDocument) Index() string {
 }
 
 type BleveSearchResponse struct {
-	Result    *bleveSearch.SearchResult
-	Documents []iface.IDocument
+	Result *bleveSearch.SearchResult
 }
 
 func (b *BleveSearchResponse) Total() uint64 {
@@ -43,7 +160,23 @@ func (b *BleveSearchResponse) Took() time.Duration {
 }
 
 func (b *BleveSearchResponse) Hits() []iface.IDocument {
-	return b.Documents
+	documents := make([]iface.IDocument, 0, len(b.Result.Hits))
+	for _, doc := range b.Result.Hits {
+		documents = append(documents, &BleveSearchDocument{DocMatch: doc})
+	}
+	return documents
+}
+
+func (b *BleveSearchResponse) Facets() []iface.IFacet {
+	if len(b.Result.Facets) == 0 {
+		return nil
+	}
+
+	facets := make([]iface.IFacet, 0, len(b.Result.Facets))
+	for name, doc := range b.Result.Facets {
+		facets = append(facets, &BleveFacet{mName: name, mFacetResult: doc})
+	}
+	return facets
 }
 
 type BleveSearchResult struct {
@@ -79,13 +212,7 @@ func (b *BleveSearchResult) ToBytes() []byte {
 }
 
 func (b *BleveSearchResult) ToSearchResponse() iface.ISearchResponse {
-	documents := make([]iface.IDocument, 0, len(b.Result.Hits))
-	for _, doc := range b.Result.Hits {
-		documents = append(documents, &BleveSearchDocument{DocMatch: doc})
-	}
-
 	return &BleveSearchResponse{
-		Result:    b.Result,
-		Documents: documents,
+		Result: b.Result,
 	}
 }
