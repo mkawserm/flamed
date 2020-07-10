2f025063ea504afd7974b04fa4db4d1cf9299e8d
kawser <mkawserm@gmail.com>
2020-07-10 12:47:52 +0600

    TEST SUITE REFACTORED

--- testsuite/storage/state.go
+++ testsuite/storage/state.go
@@ -7,6 +7,79 @@ import (
 	"testing"
 )
 
+func setKeyValuePair(t *testing.T, stateStorage iface.IStateStorage, inputDataTable []string) {
+	txn := stateStorage.NewTransaction()
+	if txn == nil {
+		t.Fatal("unexpected nil pointer")
+		return
+	}
+	for _, v := range inputDataTable {
+		if err := txn.Set([]byte(v), []byte(v)); err != nil {
+			t.Fatal("unexpected error: ", err)
+			return
+		}
+	}
+
+	if err := txn.Commit(); err != nil {
+		t.Fatal("unexpected error: ", err)
+		return
+	}
+}
+
+func forwardIteratorCheck(t *testing.T, stateStorage iface.IStateStorage, expectedForwardDataTable []string) {
+	txn := stateStorage.NewTransaction()
+	forwardIterator := txn.ForwardIterator()
+	if forwardIterator == nil {
+		t.Fatal("unexpected nil forward iterator")
+		return
+	}
+
+	var i = 0
+	for forwardIterator.Rewind(); forwardIterator.Valid(); forwardIterator.Next() {
+		state := forwardIterator.StateSnapshot()
+		currentData := expectedForwardDataTable[i]
+
+		if !bytes.EqualFold([]byte(currentData), state.Address) {
+			t.Fatal("address ordering is not correct")
+		}
+
+		if !bytes.EqualFold([]byte(currentData), state.Data) {
+			t.Fatal("data mismatch")
+		}
+
+		i = i + 1
+	}
+
+	forwardIterator.Close()
+}
+
+func reverseIteratorCheck(t *testing.T, stateStorage iface.IStateStorage, expectedReverseDataTable []string) {
+	txn := stateStorage.NewTransaction()
+	reverseIterator := txn.ReverseIterator()
+	if reverseIterator == nil {
+		t.Error("unexpected nil reverse iterator")
+		return
+	}
+
+	var i = 0
+	for reverseIterator.Rewind(); reverseIterator.Valid(); reverseIterator.Next() {
+		state := reverseIterator.StateSnapshot()
+		currentData := expectedReverseDataTable[i]
+
+		if !bytes.EqualFold([]byte(currentData), state.Address) {
+			t.Fatal("address ordering is not correct")
+		}
+
+		if !bytes.EqualFold([]byte(currentData), state.Data) {
+			t.Fatal("data mismatch")
+		}
+
+		i = i + 1
+	}
+
+	reverseIterator.Close()
+}
+
 func StateStorageTestSuite(t *testing.T, stateStorage iface.IStateStorage) {
 	path := "/tmp/test_db_1"
 	defer func() {
@@ -22,13 +95,6 @@ func StateStorageTestSuite(t *testing.T, stateStorage iface.IStateStorage) {
 	defer func() {
 		_ = stateStorage.Close()
 	}()
-
-	txn := stateStorage.NewTransaction()
-
-	if txn == nil {
-		t.Fatal("unexpected nil pointer")
-		return
-	}
 
 	inputDataTable := []string{
 		"z",
@@ -72,62 +138,7 @@ func StateStorageTestSuite(t *testing.T, stateStorage iface.IStateStorage) {
 		"0",
 	}
 
-	for _, v := range inputDataTable {
-		if err := txn.Set([]byte(v), []byte(v)); err != nil {
-			t.Fatal("unexpected error: ", err)
-			return
-		}
-	}
-
-	if err := txn.Commit(); err != nil {
-		t.Fatal("unexpected error: ", err)
-		return
-	}
-
-	txn = stateStorage.NewTransaction()
-	forwardIterator := txn.ForwardIterator()
-	if forwardIterator == nil {
-		t.Fatal("unexpected nil forward iterator")
-		return
-	}
-	var i = 0
-	for forwardIterator.Rewind(); forwardIterator.Valid(); forwardIterator.Next() {
-		state := forwardIterator.StateSnapshot()
-		currentData := expectedForwardDataTable[i]
-
-		if !bytes.EqualFold([]byte(currentData), state.Address) {
-			t.Fatal("address ordering is not correct")
-		}
-
-		if !bytes.EqualFold([]byte(currentData), state.Data) {
-			t.Fatal("data mismatch")
-		}
-
-		i = i + 1
-	}
-
-	forwardIterator.Close()
-
-	reverseIterator := txn.ReverseIterator()
-	if reverseIterator == nil {
-		t.Error("unexpected nil reverse iterator")
-		return
-	}
-	i = 0
-	for reverseIterator.Rewind(); reverseIterator.Valid(); reverseIterator.Next() {
-		state := reverseIterator.StateSnapshot()
-		currentData := expectedReverseDataTable[i]
-
-		if !bytes.EqualFold([]byte(currentData), state.Address) {
-			t.Fatal("address ordering is not correct")
-		}
-
-		if !bytes.EqualFold([]byte(currentData), state.Data) {
-			t.Fatal("data mismatch")
-		}
-
-		i = i + 1
-	}
-
-	reverseIterator.Close()
+	setKeyValuePair(t, stateStorage, inputDataTable)
+	forwardIteratorCheck(t, stateStorage, expectedForwardDataTable)
+	reverseIteratorCheck(t, stateStorage, expectedReverseDataTable)
 }
