4c7a6e329a830a3c80edd6f25f8d8de19c265b7e
kawser <mkawserm@gmail.com>
2020-07-10 17:04:24 +0600

    COMMENT ADDED TO THE CLUSTER INTERFACE

--- pkg/iface/cluster.go
+++ pkg/iface/cluster.go
@@ -1,9 +1,19 @@
 package iface
 
 type IOnDiskClusterConfiguration interface {
+	// Join defines the join flag for on disk cluster
+	// configuration
 	Join() bool
+
+	// ClusterID method contains cluster id
 	ClusterID() uint64
+
+	// ClusterName method contains cluster name
 	ClusterName() string
+
+	// InitialMembers method defines all initial members of the cluster
 	InitialMembers() map[uint64]string
+
+	// StateMachine method return a method which returns storaged state machine
 	StateMachine(IStoragedConfiguration) func(uint64, uint64) IStoraged
 }
